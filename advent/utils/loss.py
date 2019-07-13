516ee50e2ebad65959f89ccf9edebee649e38f3a
tuanhungvu <tuanhungvu@gmail.com>
2019-07-13 16:22:46 +0200

    release ADVENT

--- advent/utils/loss.py
+++ advent/utils/loss.py
@@ -35,4 +35,4 @@ def entropy_loss(v):
     """
     assert v.dim() == 4
     n, c, h, w = v.size()
-    return -torch.sum(torch.mul(v, torch.log2(v + 1e-30))) / (h * w * np.log2(c))
+    return -torch.sum(torch.mul(v, torch.log2(v + 1e-30))) / (n * h * w * np.log2(c))
